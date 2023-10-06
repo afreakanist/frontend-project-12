0c083887d15104414644425e7acfe6cc34c6795a
Anna Korotkikh <afreakanist@gmail.com>
2023-10-06 16:05:32 +0600

    step 2: adding basic routing (main, login, not found)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,16 @@
-const App = () => <h1>CHAT!!!</h1>;
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Main from './components/Main';
+import NotFound from './components/NotFound';
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="login" element={<Login />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ frontend/src/components/Main.js
@@ -0,0 +1,3 @@
+const Main = () => <h1>Main page</h1>;
+
+export default Main;
