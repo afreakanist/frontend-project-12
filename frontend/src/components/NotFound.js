5e036e36c7e4da555bd99ab770e409e0cfbe6d0f
Anna Korotkikh <afreakanist@gmail.com>
2023-10-09 21:19:30 +0600

    improving and styling Not Found page

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,3 +1,17 @@
-const NotFound = () => <h1>Page not found</h1>;
+import { Link } from 'react-router-dom';
+
+const NotFound = () => (
+  <main className="not-found">
+    <div className="not-found__wrapper">
+      <div className="not-found__error">
+        <h1 className="not-found__status">404</h1>
+        <p className="not-found__message">Page not found</p>
+      </div>
+      <Link to="/" className="not-found__link">
+        Go to main page
+      </Link>
+    </div>
+  </main>
+);
 
 export default NotFound;
