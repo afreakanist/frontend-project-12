9d37e0c94ecdf8e5067fb2cca1fa27327e0a60b1
Anna Korotkikh <afreakanist@gmail.com>
2023-10-22 23:29:23 +0600

    using svg icons as components

--- frontend/src/components/MessageForm.js
+++ frontend/src/components/MessageForm.js
@@ -1,4 +1,3 @@
-import { useContext } from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import Button from 'react-bootstrap/Button';
@@ -7,13 +6,13 @@ import Spinner from 'react-bootstrap/Spinner';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import filter from 'leo-profanity';
-
+import { useAuth } from '../hooks/useAuth';
 import { sendMessage } from '../utils/chatApi';
-import CurrentUserCtx from '../contexts/CurrentUserCtx';
+import { ReactComponent as SendMessageIcon } from '../assets/arrow.svg';
 
 const MessageForm = () => {
   const { currentChannelId } = useSelector((state) => state.channels);
-  const { username } = useContext(CurrentUserCtx);
+  const { user: { username } } = useAuth();
   const { t } = useTranslation();
 
   const formik = useFormik({
@@ -75,7 +74,7 @@ const MessageForm = () => {
             )
             : (
               <>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor"><path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" /></svg>
+                <SendMessageIcon />
                 <span className="visually-hidden">{t('messageForm.sendBtn')}</span>
               </>
             )}
--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -7,6 +7,7 @@ import ListGroup from 'react-bootstrap/ListGroup';
 
 import { actions as channelsActions } from '../slices/channelsSlice';
 import { actions as modalActions } from '../slices/modalSlice';
+import { ReactComponent as AddChannelIcon } from '../assets/plus.svg';
 
 const Sidebar = () => {
   const { currentChannelId, channels } = useSelector((state) => state.channels);
@@ -31,10 +32,7 @@ const Sidebar = () => {
           className="bg-transparent p-0 border-0 shadow-none text-dark"
           onClick={() => handleShowModal('new')}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="24" height="24" fill="currentColor">
-            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
-            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
-          </svg>
+          <AddChannelIcon />
         </Button>
       </div>
       <ListGroup variant="flush" as="ul" className="channels-list overflow-hidden-scroll">
