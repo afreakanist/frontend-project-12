9c70ef92f7c95fc13bf9e5e966cd5f6e7a897a75
Anna Korotkikh <afreakanist@gmail.com>
2023-11-07 19:29:41 +0600

    filtering current channel messages in selector

--- frontend/src/components/ChannelView.js
+++ frontend/src/components/ChannelView.js
@@ -6,7 +6,8 @@ import MessageForm from './MessageForm';
 
 const ChannelView = () => {
   const { currentChannelId } = useSelector((state) => state.channels);
-  const { messages } = useSelector((state) => state.messages);
+  const messages = useSelector((state) => state.messages.messages
+    .filter((message) => message.channelId === currentChannelId));
   const { t } = useTranslation();
   const anchorElem = useRef(null);
 
@@ -21,23 +22,21 @@ const ChannelView = () => {
 
   return (
     <div className="channel-view overflow-hidden-scroll h-100 d-flex flex-column justify-content-end">
-      {messages?.filter((message) => message.channelId === currentChannelId).length > 0
+      {messages.length
         ? (
           <ListGroup variant="flush" as="ul" className="overflow-hidden-scroll">
-            {messages
-              .filter((message) => message.channelId === currentChannelId)
-              .map((message) => (
-                <ListGroup.Item
-                  key={`message-${message.id}`}
-                  as="li"
-                  className="bg-transparent border-0"
-                >
-                  <div className="text-break mb-2">
-                    <span className="fw-bold">{`${message.username}: `}</span>
-                    {message.body}
-                  </div>
-                </ListGroup.Item>
-              ))}
+            {messages.map((message) => (
+              <ListGroup.Item
+                key={`message-${message.id}`}
+                as="li"
+                className="bg-transparent border-0"
+              >
+                <div className="text-break mb-2">
+                  <span className="fw-bold">{`${message.username}: `}</span>
+                  {message.body}
+                </div>
+              </ListGroup.Item>
+            ))}
             <div ref={anchorElem} />
           </ListGroup>
         )
