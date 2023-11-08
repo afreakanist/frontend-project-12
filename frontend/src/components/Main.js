b41cfe98e9721c91f2517dd06e2884ba23549eb0
Anna Korotkikh <afreakanist@gmail.com>
2023-11-08 21:55:29 +0600

    using redux thunk to fetch and store channels and messages instead of dispatching manually

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -2,35 +2,25 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
 import useAuth from '../hooks/useAuth';
-import { getData } from '../utils/api';
-import { actions as channelsActions } from '../slices/channelsSlice';
-import { actions as messagesActions } from '../slices/messagesSlice';
+import { getChatData } from '../slices/channelsSlice';
 import ChannelView from './ChannelView';
 import Sidebar from './Sidebar';
 import Modal from './Modal';
 
 const Main = () => {
   const {
-    user, requestError, setRequestError, handleError, buildAuthHeaders,
+    user, requestError, handleError, buildAuthHeaders,
   } = useAuth();
   const dispatch = useDispatch();
 
-  const getChatData = () => {
-    getData(buildAuthHeaders())
-      .then(({ channels, messages }) => {
-        setRequestError(null);
-        dispatch(channelsActions.setChannels(channels));
-        dispatch(messagesActions.setMessages(messages));
-      })
-      .catch((error) => {
-        handleError(error, 'getData');
-        toast.error(requestError?.key, requestError?.values);
-      });
-  };
-
   useEffect(() => {
     if (user?.isLoggedIn) {
-      getChatData();
+      dispatch(getChatData(buildAuthHeaders()))
+        .unwrap()
+        .catch((error) => {
+          handleError(error, 'getData');
+          toast.error(requestError?.key, requestError?.values);
+        });
     }
   }, [user?.isLoggedIn]);
 
